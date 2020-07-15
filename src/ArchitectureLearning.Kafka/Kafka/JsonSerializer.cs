058d5c623f538cf2cb88153fc87367999faf35c9
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-07-15 10:10:45 +0100

    Implementing kafka

---
+++ src/ArchitectureLearning.Kafka/Kafka/JsonSerializer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace ArchitectureLearning.Kafka.Kafka
+{
+    public class JsonSerializer<T> : ISerializer<T>
+    {
+        public byte[] Serialize(T data, SerializationContext context)
+        {
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data));
+        }
+    }
+    
+    public class JsonDeserializer<T> : IDeserializer<T>
+    {
+        public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+        {
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data));
+        }
+    }
+}
---
+++ src/ArchitectureLearning.Kafka/Kafka/KafkaOptions.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace ArchitectureLearning.Kafka.Kafka
+{
+    public class KafkaOptions
+    {
+        public IDictionary<string, string> Configuration { get; set; }
+    }
+}
